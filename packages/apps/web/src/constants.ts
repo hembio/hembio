22c96c6c0b5b427f389842f70affd2742e40a7c7
Marcus Ekwall <marcus.ekwall@gmail.com>
2021-07-23 15:19:58 +0200

    feat: add suppport for vite

--- packages/apps/web/src/constants.ts
+++ packages/apps/web/src/constants.ts
@@ -1,4 +1,8 @@
+const isVite = !!import.meta;
+const env = import.meta ? import.meta.env : process.env;
 export const HEMBIO_API_URL =
-  process.env.REACT_APP_HEMBIO_API_URL || "http://localhost:4000";
+  (isVite ? env.VITE_HEMBIO_API_URL : env.REACT_APP_HEMBIO_API_URL) ||
+  "http://localhost:4000";
 export const HEMBIO_WS_URL =
-  process.env.REACT_APP_HEMBIO_WS_URL || "ws://localhost:4000";
+  (isVite ? env.VITE_HEMBIO_WS_URL : env.REACT_APP_HEMBIO_WS_URL) ||
+  "ws://localhost:4000";
---
+++ packages/apps/web/vite.config.ts
@@ -0,0 +1,53 @@
+import path from "path";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
+  plugins: [reactRefresh()],
+  build: {
+    outDir: "./dist",
+  },
+  server: {
+    port: 3000,
+    open: false,
+    base: "https://hembio.local:3443",
+    hmr: {
+      port: 3443,
+    },
+  },
+  resolve: {
+    alias: {
+      "~": path.resolve("./src"),
+      $: path.resolve("./public"),
+    },
+  },
+  // optimizeDeps: {
+  //   include: [
+  //     "fast-json-stable-stringify",
+  //     "@apollo/client",
+  //     "zen-observable",
+  //     "@apollo/client/core",
+  //     "@apollo/client/cache",
+  //     "@apollo/client/link/ws",
+  //     "@apollo/client/link/context",
+  //     "@apollo/client/utilities",
+  //     "@material-ui/core",
+  //     "@material-ui/core/colors",
+  //   ],
+  //   exclude: [
+  //     "@mikro-orm/core",
+  //     "@mikro-orm/reflection",
+  //     "@mikro-orm/sqlite",
+  //     "reflect-metadata",
+  //     "dotenv",
+  //     "uuid",
+  //     "bcrypt",
+  //     "graphql",
+  //     "type-graphql",
+  //     "slug",
+  //   ],
+  // },
+});
