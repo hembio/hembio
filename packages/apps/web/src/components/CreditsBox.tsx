16177770ade515810ec1aa6580a6783ff72c896e
Marcus Ekwall <marcus.ekwall@gmail.com>
2021-07-23 16:30:01 +0200

    fix(apps/web): CreditsBox rendering

--- packages/apps/web/src/components/CreditsBox.tsx
+++ packages/apps/web/src/components/CreditsBox.tsx
@@ -22,46 +22,7 @@ export function CreditsBox({ title }: CreditBoxProps): JSX.Element {
     skip: !show,
   });
 
-  if (!show) {
-    const topBilling = title ? title.topBilling : new Array(6).fill(undefined);
-    return (
-      <Paper>
-        <Grid
-          container
-          sx={{ p: 4, pt: 2, mt: 6 }}
-          flexDirection="row"
-          spacing={2}
-        >
-          <Grid item sx={{ mb: 2 }} flexGrow={5}>
-            <Grid container>
-              <Grid item xs flexGrow={6.8}>
-                <Typography variant="h5">Top cast</Typography>
-              </Grid>
-              {title && topBilling.length > 0 && (
-                <Grid item xs>
-                  <Button
-                    variant="text"
-                    color="primary"
-                    size="medium"
-                    onClick={() => setShow(!show)}
-                  >
-                    {show ? "Only Top Cast" : "All Cast & Crew"}
-                  </Button>
-                </Grid>
-              )}
-            </Grid>
-            <Grid container flexDirection="row" spacing={2}>
-              {topBilling.map((credit, idx) => (
-                <Grid item key={!credit ? idx : credit.id}>
-                  <CreditListitem credit={credit} />
-                </Grid>
-              ))}
-            </Grid>
-          </Grid>
-        </Grid>
-      </Paper>
-    );
-  }
+  const topBilling = title ? title.topBilling : new Array(6).fill(undefined);
 
   const cast = data?.title?.cast || new Array(12).fill(undefined);
   const crew = data?.title?.crew || [];
@@ -89,60 +50,78 @@ export function CreditsBox({ title }: CreditBoxProps): JSX.Element {
         flexDirection="row"
         spacing={2}
       >
-        {[
-          "Cast",
-          "Writing",
-          "Directing",
-          "Production",
-          "Editing",
-          "Camera",
-          "Art",
-          "Sound",
-          "Lighting",
-          "Visual Effects",
-          "Acting",
-          "Costume & Make-Up",
-          "Crew",
-        ].map((depName) => {
-          const credits = departments[depName];
-          if (!credits || credits.length === 0) {
-            return;
-          }
-          return (
-            <Grid
-              key={depName}
-              item
-              xs={depName === "Cast" ? 12 : undefined}
-              sx={{ mb: 2 }}
-              flexGrow={10}
-            >
-              <Grid container>
-                <Grid item xs flexGrow={7.6}>
-                  <Typography variant="h5">{depName}</Typography>
-                </Grid>
-                {depName === "Cast" && (
-                  <Grid item xs>
-                    <Button
-                      variant="text"
-                      color="primary"
-                      size="medium"
-                      onClick={() => setShow(!show)}
-                    >
-                      {show ? "Only Top Cast" : "All Cast & Crew"}
-                    </Button>
-                  </Grid>
-                )}
-              </Grid>
-              <Grid container flexDirection="row" spacing={2} sx={{ mt: 0 }}>
-                {credits.map((credit, idx) => (
-                  <Grid item key={!credit ? idx : credit.id}>
-                    <CreditListitem credit={credit} />
-                  </Grid>
-                ))}
-              </Grid>
+        <Grid item sx={{ mb: 2 }} flexGrow={10}>
+          <Grid container>
+            <Grid item xs flexGrow={6.8}>
+              <Typography variant="h5">Top cast</Typography>
             </Grid>
-          );
-        })}
+            {title && topBilling.length > 0 && (
+              <Grid item xs>
+                <Button
+                  variant="text"
+                  color="primary"
+                  size="medium"
+                  onClick={() => setShow(!show)}
+                >
+                  {show ? "Only Top Cast" : "All Cast & Crew"}
+                </Button>
+              </Grid>
+            )}
+          </Grid>
+          {!show && (
+            <Grid container flexDirection="row" spacing={2} sx={{ mt: 0 }}>
+              {topBilling.map((credit, idx) => (
+                <Grid item key={!credit ? idx : credit.id}>
+                  <CreditListitem credit={credit} />
+                </Grid>
+              ))}
+            </Grid>
+          )}
+          {show &&
+            [
+              "Cast",
+              "Writing",
+              "Directing",
+              "Production",
+              "Editing",
+              "Camera",
+              "Art",
+              "Sound",
+              "Lighting",
+              "Visual Effects",
+              "Acting",
+              "Costume & Make-Up",
+              "Crew",
+            ].map((depName) => {
+              const credits = departments[depName];
+              if (!credits || credits.length === 0) {
+                return;
+              }
+              return (
+                <>
+                  {depName !== "Cast" && (
+                    <Grid container>
+                      <Grid item xs flexGrow={7.6}>
+                        <Typography variant="h5">{depName}</Typography>
+                      </Grid>
+                    </Grid>
+                  )}
+                  <Grid
+                    container
+                    flexDirection="row"
+                    spacing={2}
+                    sx={{ mt: 0 }}
+                  >
+                    {credits.map((credit, idx) => (
+                      <Grid item key={!credit ? idx : credit.id}>
+                        <CreditListitem credit={credit} />
+                      </Grid>
+                    ))}
+                  </Grid>
+                </>
+              );
+            })}
+        </Grid>
       </Grid>
     </Paper>
   );
