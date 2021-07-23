c8c89bdb2a03824b3551f3813ff899d182c9a525
Marcus Ekwall <marcus.ekwall@gmail.com>
2021-07-24 01:30:36 +0200

    chore(indexer): remove unused imports

--- packages/services/indexer/src/metadata/metadata.module.ts
+++ packages/services/indexer/src/metadata/metadata.module.ts
@@ -1,30 +1,10 @@
-import {
-  CreditEntity,
-  FileEntity,
-  GenreEntity,
-  LibraryEntity,
-  MikroOrmModule,
-  PersonEntity,
-  TaskModule,
-  TitleEntity,
-} from "@hembio/core";
+import { TaskModule } from "@hembio/core";
 import { Module } from "@nestjs/common";
 import { ScheduleModule } from "@nestjs/schedule";
 import { MetadataService } from "./metadata.service";
 
 @Module({
-  imports: [
-    MikroOrmModule.forFeature([
-      LibraryEntity,
-      TitleEntity,
-      FileEntity,
-      GenreEntity,
-      PersonEntity,
-      CreditEntity,
-    ]),
-    ScheduleModule.forRoot(),
-    TaskModule,
-  ],
+  imports: [ScheduleModule.forRoot(), TaskModule],
   providers: [MetadataService],
   exports: [MetadataService],
 })
