f867f447066ef69ab7c9a2760bd71a9fd0fb90e4
Marcus Ekwall <marcus.ekwall@gmail.com>
2021-07-23 16:29:32 +0200

    feat(i18n): add chained backend

--- packages/apps/web/src/i18n.ts
+++ packages/apps/web/src/i18n.ts
@@ -1,32 +1,41 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import ChainedBackend from "i18next-chained-backend";
 import resourcesToBackend from "i18next-resources-to-backend";
 import { initReactI18next } from "react-i18next";
 
 i18n
-  .use(
-    resourcesToBackend((language, namespace, callback) => {
-      import(`./locales/${language}/${namespace}.json`)
-        .then((resources) => {
-          callback(null, resources);
-        })
-        .catch((error) => {
-          callback(error, null);
-        });
-    }),
-  )
+  .use(ChainedBackend)
   .use(LanguageDetector)
   .use(initReactI18next) // bind react-i18next to the instance
   .init({
     fallbackLng: "en-US",
-    fallbackNS: "ns1",
-    defaultNS: "ns1",
+    fallbackNS: "translation",
+    defaultNS: "translation",
     debug: true,
     interpolation: {
-      escapeValue: false, // not needed for react!!
+      escapeValue: false, // not needed for react
     },
     react: {
       useSuspense: false,
+    },
+    backend: {
+      backends: [
+        resourcesToBackend((language, namespace, callback) => {
+          import(`./locales/${language}/${namespace}.json`)
+            .then((resources) => {
+              callback(null, resources);
+            })
+            .catch((error) => {
+              callback(error, null);
+            });
+        }),
+      ],
+      backendOptions: [
+        {
+          loadPath: "./locales/{{lng}}/{{ns}}.json",
+        },
+      ],
     },
   });
 
