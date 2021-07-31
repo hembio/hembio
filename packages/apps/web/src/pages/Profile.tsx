0f945208672d2e8364463d6e6fb88de10c882aea
Marcus Ekwall <marcus.ekwall@gmail.com>
2021-07-31 14:12:25 +0200

    fix: add missing alt prop

--- packages/apps/web/src/pages/Profile.tsx
+++ packages/apps/web/src/pages/Profile.tsx
@@ -5,7 +5,7 @@ export function Profile(): JSX.Element {
   const { authStore } = useStores();
   return (
     <Container>
-      <img src={authStore.qrCode} />
+      <img src={authStore.qrCode} alt="TFA QR code" />
     </Container>
   );
 }
