71844f16bd93c6596cbf49e404542c424f747288
Marcus Ekwall <marcus.ekwall@gmail.com>
2021-07-31 10:58:16 +0200

    fix(eslint): overrides slows it down to a crawl

--- packages/configs/eslint/index.js
+++ packages/configs/eslint/index.js
@@ -1,134 +1,91 @@
+const path = require("path");
+
 /** @type {import('eslint').Linter.Config} */
 const config = {
   root: true,
+  plugins: ["unused-imports", "workspaces", "@emotion"],
+  extends: [
+    "eslint:recommended",
+    "plugin:json/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:react/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
   env: {
     es6: true,
     node: true,
   },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: path.join(
+          __dirname,
+          "../../../",
+          "packages/**/*",
+          "tsconfig.json",
+        ),
+      },
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "no-unused-vars": "off",
+    "no-empty": "warn",
+    "import/no-unresolved": "error",
+    "import/order": [
+      "warn",
+      {
+        "newlines-between": "never",
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "object",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        alphabetize: {
+          order: "asc",
+        },
+      },
+    ],
+    "unused-imports/no-unused-imports": "warn",
+    "unused-imports/no-unused-vars": "off",
+    "@typescript-eslint/explicit-member-accessibility": ["warn"],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "react/display-name": "off",
+    "@emotion/jsx-import": "off",
+    "@emotion/no-vanilla": "error",
+    "@emotion/import-from-emotion": "error",
+    "@emotion/styled-import": "error",
+  },
   overrides: [
     {
-      files: ["*.json"],
-      extends: ["plugin:json/recommended"],
-    },
-    {
       files: ["*.{js,jsx}"],
-      plugins: ["unused-imports", "workspaces"],
-      extends: ["eslint:recommended", "plugin:import/recommended", "prettier"],
-      settings: {
-        react: {
-          version: "detect",
-        },
-      },
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: "module",
-        ecmaFeatures: {
-          jsx: true,
-          modules: true,
-        },
-      },
       rules: {
         "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-        "no-empty": "warn",
-        "import/no-unresolved": "error",
-        "import/order": [
-          "warn",
-          {
-            "newlines-between": "never",
-            groups: [
-              "builtin",
-              "external",
-              "internal",
-              "object",
-              "parent",
-              "sibling",
-              "index",
-            ],
-            alphabetize: {
-              order: "asc",
-            },
-          },
-        ],
-        "unused-imports/no-unused-imports": "warn",
-        "unused-imports/no-unused-vars": "off",
-        "react/react-in-jsx-scope": "off",
-        "react/prop-types": "off",
-        "react/display-name": "off",
-        "@emotion/jsx-import": "error",
-        "@emotion/no-vanilla": "error",
-        "@emotion/import-from-emotion": "error",
-        "@emotion/styled-import": "error",
-      },
-    },
-    {
-      files: ["*.{ts,tsx}"],
-      plugins: ["unused-imports", "workspaces", "@emotion"],
-      parser: "@typescript-eslint/parser",
-      extends: [
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:import/recommended",
-        "plugin:import/typescript",
-        "prettier",
-      ],
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: "module",
-        ecmaFeatures: {
-          jsx: true,
-          modules: true,
-        },
-      },
-      settings: {
-        react: {
-          version: "detect",
-        },
-        "import/parsers": {
-          "@typescript-eslint/parser": [".ts", ".tsx"],
-        },
-        "import/resolver": {
-          typescript: {
-            alwaysTryTypes: true,
-            project: ["packages/**/*/tsconfig.json"],
-          },
-        },
-      },
-      rules: {
-        "no-empty": "warn",
-        "import/no-unresolved": "error",
-        "import/order": [
-          "warn",
-          {
-            "newlines-between": "never",
-            groups: [
-              "builtin",
-              "external",
-              "internal",
-              "object",
-              "parent",
-              "sibling",
-              "index",
-            ],
-            alphabetize: {
-              order: "asc",
-            },
-          },
-        ],
-        "unused-imports/no-unused-imports": "warn",
-        "unused-imports/no-unused-vars": "off",
-        "react/react-in-jsx-scope": "off",
-        "react/prop-types": "off",
-        "react/display-name": "off",
-        "@emotion/jsx-import": "error",
-        "@emotion/no-vanilla": "error",
-        "@emotion/import-from-emotion": "error",
-        "@emotion/styled-import": "error",
-        "@typescript-eslint/explicit-member-accessibility": ["warn"],
-        "@typescript-eslint/no-unused-vars": [
-          "warn",
-          { argsIgnorePattern: "^_" },
-        ],
+        "@typescript-eslint/no-var-requires": "off",
       },
     },
     {
