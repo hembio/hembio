616bfde0f9be162c0dd4d29c26267ebfa5f8a59d
Marcus Ekwall <marcus.ekwall@gmail.com>
2021-07-31 08:17:34 +0200

    chore(eslint): improve rules

--- packages/configs/eslint/index.js
+++ packages/configs/eslint/index.js
@@ -1,79 +1,109 @@
 /** @type {import('eslint').Linter.Config} */
 const config = {
   root: true,
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-  ],
-  plugins: ["unused-imports"],
   env: {
-    node: true,
-    commonjs: true,
-    browser: true,
     es6: true,
-    jest: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-      modules: true,
-    },
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"],
-    },
-    "import/resolver": {
-      typescript: {
-        alwaysTryTypes: true,
-        project: ["packages/*/tsconfig.json", "packages/apps/*/tsconfig.json"],
-      },
-    },
-  },
-  rules: {
-    "no-empty": "warn",
-    "import/no-unresolved": "error",
-    "import/order": [
-      "warn",
-      {
-        "newlines-between": "never",
-        groups: [
-          "builtin",
-          "external",
-          "internal",
-          "object",
-          "parent",
-          "sibling",
-          "index",
-        ],
-        alphabetize: {
-          order: "asc",
-        },
-      },
-    ],
-    "unused-imports/no-unused-imports": "warn",
-    "unused-imports/no-unused-vars": "off",
-    "@typescript-eslint/explicit-member-accessibility": ["warn"],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "react/display-name": "off",
+    node: true,
   },
   overrides: [
     {
       files: ["*.js"],
-      rules: {
-        "@typescript-eslint/no-var-requires": "off",
+      extends: ["eslint:recommended", "prettier"],
+    },
+    {
+      files: ["*.json"],
+      extends: ["plugin:json/recommended"],
+    },
+    {
+      files: ["*.{js,jsx,ts,tsx}"],
+      plugins: ["unused-imports", "workspaces"],
+      extends: ["plugin:import/recommended"],
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+          modules: true,
+        },
       },
+      settings: {
+        react: {
+          version: "detect",
+        },
+        "import/parsers": {
+          "@typescript-eslint/parser": [".ts", ".tsx"],
+        },
+        "import/resolver": {
+          typescript: {
+            alwaysTryTypes: true,
+            project: ["packages/**/*/tsconfig.json"],
+          },
+        },
+      },
+      rules: {
+        "no-empty": "warn",
+        "import/no-unresolved": "error",
+        "import/order": [
+          "warn",
+          {
+            "newlines-between": "never",
+            groups: [
+              "builtin",
+              "external",
+              "internal",
+              "object",
+              "parent",
+              "sibling",
+              "index",
+            ],
+            alphabetize: {
+              order: "asc",
+            },
+          },
+        ],
+        "unused-imports/no-unused-imports": "warn",
+        "unused-imports/no-unused-vars": "off",
+      },
+    },
+    {
+      files: ["*.{ts,tsx}"],
+      plugins: ["unused-imports", "workspaces"],
+      parser: "@typescript-eslint/parser",
+      extends: [
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/recommended",
+        "plugin:import/typescript",
+        "prettier",
+      ],
+      rules: {
+        "@typescript-eslint/explicit-member-accessibility": ["warn"],
+        "@typescript-eslint/no-unused-vars": [
+          "warn",
+          { argsIgnorePattern: "^_" },
+        ],
+      },
+    },
+    {
+      files: ["*.{jsx,tsx}"],
+      plugins: ["@emotion"],
+      extends: ["plugin:react/recommended"],
+      rules: {
+        "react/react-in-jsx-scope": "off",
+        "react/prop-types": "off",
+        "react/display-name": "off",
+        "@emotion/jsx-import": "error",
+        "@emotion/no-vanilla": "error",
+        "@emotion/import-from-emotion": "error",
+        "@emotion/styled-import": "error",
+      },
+    },
+    {
+      files: ["**/__tests__/*.{spec,test}.{js,jsx,ts,tsx}"],
+      env: {
+        jest: true,
+      },
+      extends: ["plugin:jest/recommended"],
     },
   ],
 };
