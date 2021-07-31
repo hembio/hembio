7644e444c329d7064c2d1a3f112b3a036d61a5ee
Marcus Ekwall <marcus.ekwall@gmail.com>
2021-07-31 11:06:41 +0200

    chore: fix import in declaration

--- packages/apps/web/src/react-i18next.d.ts
+++ packages/apps/web/src/react-i18next.d.ts
@@ -1,7 +1,7 @@
 // import the original type declarations
 import "react-i18next";
 // import all namespaces (for the default language, only)
-import ns1 from "~/locales/en-US/ns1.json";
+import ns1 from "~/locales/en-US/translation.json";
 
 // react-i18next versions lower than 11.11.0
 declare module "react-i18next" {
