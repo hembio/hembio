7167e202ff8d873b24c4f043bcb460419e1f716f
Marcus Ekwall <marcus.ekwall@gmail.com>
2021-08-01 07:04:46 +0200

    fix(matchTitleYear): improper matching of input with comma

--- packages/services/indexer/src/utils/matchTitleYear.ts
+++ packages/services/indexer/src/utils/matchTitleYear.ts
@@ -1,4 +1,4 @@
-const regexp = /(?<title>[\p{L}\-\d.!?'#&+*½@·$%:()<> ]+) \((?<year>\d{4})\)/u;
+const regexp = /(?<title>[\p{L}\-\d.,!?'#&+*½@·$%:()<> ]+) \((?<year>\d{4})\)/u;
 
 export function matchTitleYear(str: string): RegExpMatchArray | null {
   return regexp.exec(str);
